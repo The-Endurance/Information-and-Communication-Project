ee1c6b9e3bdf600735e1d3dd698889d59c64af83
KarthikL1729 <lakshmanan.lakshmanan@students.iiit.ac.in>
2021-07-30 01:17:08 +0530

    CODE ENCODES PROPERLY LESSGOOOOOOOOOOOOOOOOOOOOOO

--- Q1.py
+++ Q1.py
@@ -2,20 +2,21 @@
 #tx = f.read()
 #print(tx)
 #f.close()
+from heapq import *
 import collections
-
-def min_heapify(A,k):
+"""
+def min_heapify(A,k,key):
     l = left(k)
     r = right(k)
-    if l < len(A) and A[l].freq < A[k].freq:
+    if l < len(A) and key(A[l]) < key(A[k]):
         smallest = l
     else:
         smallest = k
-    if r < len(A) and A[r].freq < A[smallest].freq:
+    if r < len(A) and key(A[r]) < key(A[smallest]):
         smallest = r
     if smallest != k:
         A[k], A[smallest] = A[smallest], A[k]
-        min_heapify(A, smallest)
+        min_heapify(A, smallest, key)
 
 def left(k):
     return 2 * k + 1
@@ -23,20 +24,48 @@ def left(k):
 def right(k):
     return 2 * k + 2
 
-def build_min_heap(A):
+def build_min_heap(A, key):
     n = int((len(A)//2)-1)
     for k in range(n, -1, -1):
-        min_heapify(A,k)
+        min_heapify(A,k, key)
 #Tree
+"""
+
+class MyHeap():
+    def __init__(self):
+       self.key = lambda x:x.freq
+       self.index = 0 
+       self._data = []
+
+    def push(self, item):
+       heappush(self._data, (self.key(item), self.index, item))
+       self.index += 1
+
+    def pop(self):
+        self.index -= 1
+        return heappop(self._data)[2]
 
 class Node:
-   def __init__(self, dat, freq):
+    def __init__(self, dat, freq):
       self.left = None
       self.right = None
       self.dat = dat
       self.freq = freq
-   def PrintTree(self):
-      print(self.dat, "->", freq)
+      self.cw = ""
+    def __repr__(self):
+        return str(self.dat) + " -> " + str(self.freq)
+
+
+def codegen(head, s):
+    #print(head)
+    if head == None:
+        return
+    
+    if head.dat != "#":
+        head.cw = s
+        print(head.dat, "->", head.freq, "\t\t\t", head.cw)
+    codegen(head.left, s+"0")
+    codegen(head.right, s+"1")
 
 
 # Reversing a list using reverse()
@@ -49,28 +78,35 @@ with open('File3.txt', 'r') as info:
   total = sum(count.values())
 
 ncount = count.most_common()
-nheap = []
+nheap = MyHeap()
 dist = ncount.__len__()
 i = 0
 for i in range (dist):
-  nheap.append(Node(ncount[i][0], ncount[i][1]))
+    temp = Node(ncount[i][0], ncount[i][1])
+    nheap.push(temp)
+
+
+
+while nheap.index != 1:
+    l = nheap.pop()
+    r = nheap.pop()
+    temp = Node("#", l.freq+r.freq)
+    temp.left = l
+    temp.right = r
+    nheap.push(temp)
+
+codegen(nheap._data[0][2], "")
+#[print(n) for n in nheap._data]
+
 # Huffman
 
-build_min_heap(nheap)
+#print("spez")
+
+#build_min_heap(nheap, lambda n : n.freq)
+
 #print("hi")
-while (ncount.__len__() != 0):
+
+#while (ncount.__len__() != 0):
   #print("Hey")
-  l = Node(ncount[0][0], ncount[0][1])
-  #temp = ncount[0]
-  ncount.remove(ncount[0])
-  build_min_heap(nheap)
-  r = Node(ncount[0][0], ncount[0][1])
-  #temp = ncount[0]
-  ncount.remove(ncount[0])
-  build_min_heap(nheap)
-  join = Node('#', l.freq + r.freq)
-  join.left = l
-  join.right = r
-  nheap.append(join)
-  build_min_heap(nheap)
-print(nheap)
+
+
